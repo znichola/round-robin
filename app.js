72c08873a64873b0700daf914f10dfa264ed4a57
Nicholas Zivkovic <nicholas.zivkovic@scientificbeta.com>
2024-09-13 17:31:56 +0200

    routing and paths

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 import express from 'express';
+import path from 'path';
+
 const app = express();
 const port = 5555;
 
-app.get('/', (req, res) => {
-    res.send("Hellow Chess")
+app.use(express.json());
+
+app.use((req, res, next) => {
+console.log(`[${new Date().toISOString()}] ${req.hostname} ${req.method} ${req.url}`);
+next();
+})
+
+// sets 
+app.use(express.static(path.join(process.cwd(), 'public')));
+
+app.get('/hello', (req, res) => {
+    res.send("Hello Chess")
 });
 
+// Handle 404 errors (Page Not Found)
+app.use((req, res, next) => {
+    res.status(404).json({message: "Not found"});
+});
+
+// listen with app on the port
 app.listen(port, () => {
     console.log(`Round Robin app is listening on port ${port}`);
 });
